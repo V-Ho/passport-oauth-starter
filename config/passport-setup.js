029b9fb9b78f215671f7b15b850e5302c80f6b93
Vanessa Ho <vanessa.lg.ho@gmail.com>
2018-12-05 23:41:46 +0100

    create and store new google profile ids

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20')
 const keys = require('./keys')
+const User = require('../models/user-model')
 
 passport.use(
   new GoogleStrategy({
@@ -12,6 +13,13 @@ passport.use(
     // passport cb function
     console.log('passport cb function fired')
     console.log(profile)
-    done()
+    // save user's google profile id
+    new User({
+      username: profile.displayName,
+      googleId: profile.id
+    }).save().then((newUser) => {
+      console.log(`New User: ${newUser}`)
+    })
+    // done()
   })
 )
