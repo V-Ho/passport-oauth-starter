87690ca78c7ceed088418bf669c6d6106fc553d1
Vanessa Ho <vanessa.lg.ho@gmail.com>
2018-12-09 01:32:52 +0100

    serialize and deserialize user cookie

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,6 +3,18 @@ const GoogleStrategy = require('passport-google-oauth20')
 const keys = require('./keys')
 const User = require('../models/user-model')
 
+// stores user info into a cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+// cookie gets passed back to browser
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user)
+  })
+})
+
 passport.use(
   new GoogleStrategy({
     // options for Google Strategy
