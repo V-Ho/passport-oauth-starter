a32e8ee62d4c24898d7e69d6ea2bccdfe975cae4
Vanessa Ho <vanessa.lg.ho@gmail.com>
2018-12-12 23:53:23 +0100

    routes/auth-routes.js

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -10,7 +10,8 @@ router.get('/login', (req, res) => {
 // auth logout
 router.get('/logout', (req, res) => {
   // handle with passport
-  res.send('logging out')
+  req.logout()
+  res.redirect('/')
 })
 
 // auth redirects to google+ user consent screen
